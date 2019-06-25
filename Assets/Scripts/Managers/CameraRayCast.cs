7fa8f3a3ffdff38cb036825d3705efdb7850d8ac
gomezjc <gomezjc0523@gmail.com>
2019-06-24 21:09:54 -0500

    fade objects in front of player

---
+++ Assets/Scripts/Managers/CameraRayCast.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class CameraRayCast : MonoBehaviour
+{
+    RaycastHit oldHit;
+    public GameObject player;
+
+    void FixedUpdate()
+    {
+        XRay();
+    }
+
+    // Hacer a los objetos que interfieran con la vision transparentes
+    private void XRay()
+    {
+        float characterDistance = Vector3.Distance(transform.position, player.transform.position);
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, fwd, out hit, characterDistance))
+        {
+            // Si collisiono con el layer TransparentFX
+            if (hit.collider.gameObject.layer == 1)
+            {
+                oldHit = hit;
+                FadeObject(0f);
+            }
+        }else if (oldHit.collider != null)
+        {
+            FadeObject(1f);
+            oldHit = new RaycastHit();
+        }
+
+    }
+
+    private void FadeObject(float opaque)
+    {
+        Color colorA = oldHit.transform.GetComponent<MeshRenderer>().material.color;
+        colorA.a = opaque;
+        oldHit.transform.GetComponent<MeshRenderer>().material.SetColor("_Color", colorA);
+    }
+}
